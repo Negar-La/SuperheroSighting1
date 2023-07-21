1de334ad8d3cf33f5941e162ddf7f064ae5db55d
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-07-21 10:57:56 -0400

    added hero service interface + hero service impl + adjusted hero controller to access service instead of dao

---
+++ src/main/java/com/we/SuperHeroSightings/service/HeroService.java
@@ -0,0 +1,21 @@
+package com.we.SuperHeroSightings.service;
+
+import com.we.SuperHeroSightings.entities.Hero;
+import com.we.SuperHeroSightings.entities.Location;
+import com.we.SuperHeroSightings.entities.Organization;
+
+import java.util.List;
+
+public interface HeroService {
+    Hero getHeroByID(int id);
+    List<Hero> getAllHeroes();
+    Hero addHero(Hero hero);
+    public void updateHero(Hero hero);
+
+    public void deleteHeroByID(int id);
+
+    List<Hero> getHeroesByLocation(Location location);
+
+    List<Hero> getHeroesByOrganization(Organization organization);
+
+}
